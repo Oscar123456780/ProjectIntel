c24707b90df3fef5aba2a24ac00d8203bfc59b23
The Coding Press <65943881+Louis469@users.noreply.github.com>
2023-07-02 09:18:26 +0930

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,28 @@ function sendRequest() {
   paragraph.appendChild(textNode);
   chat.appendChild(paragraph);
   console.log(query);
+  fetch('https://api.pawan.krd/v1/chat/completions', {
+  method: 'POST',
+  headers: {
+    'Authorization': 'Bearer pk-***[OUR_API_KEY]***',
+    'Content-Type': 'application/json'
+  },
+  // body: '{\n    "model": "gpt-3.5-turbo",\n    "max_tokens": 100,\n    "messages": [\n        {\n            "role": "system",\n            "content": "You are an helpful assistant."\n        },\n        {\n            "role": "user",\n            "content": "Who are you?"\n        }\n    ]\n}',
+  body: JSON.stringify({
+    'model': 'gpt-3.5-turbo',
+    'max_tokens': 100,
+    'messages': [
+      {
+        'role': 'system',
+        'content': 'You are an helpful assistant.'
+      },
+      {
+        'role': 'user',
+        'content': query
+      }
+    ]
+  })
+});
   //clear input field
   document.getElementById("input").value = "";
 }
